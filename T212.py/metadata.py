701d50e6de647e9e4480eb160e07faa9a4956938
felix bruce <felixwbruce@gmail.com>
2024-02-18 12:54:19 +0000

    Changed API key so there's more data and it looks a lot better. Also changed label sizing to fix overlapping with new data

--- T212.py/metadata.py
+++ T212.py/metadata.py
@@ -6,7 +6,7 @@ fig, ax = plt.subplots()
 
 url = "https://live.trading212.com/api/v0/equity/portfolio"
 
-headers = {"Authorization": "1442967ZvodHjowzEtivuAZbeqiwHKrCxIiM"}
+headers = {"Authorization": "1288795ZihOurnSrQawlhjLLwQRyomllwNdD"}
 
 response = requests.get(url, headers=headers)
 
@@ -20,22 +20,23 @@ for tick in data:
     print(f"You own: {tick['quantity']} units of {tick['ticker']}")
     ticker = tick['ticker']
     tickers.append(ticker)
+
     average_price = float(tick['averagePrice'])
     print(f"Average Price: {average_price}")
     average_prices.append(average_price)
+
     current_price = float(tick['currentPrice'])
     print(f"Current Price: {current_price}\n")
     current_prices.append(current_price)
 
 fig, ax = plt.subplots()
-ax.plot(tickers, average_prices, c='red', alpha=0.5)
-ax.plot(tickers, current_prices, c='green', alpha=0.5)
-# plt.fill_between(tickers, average_price, current_prices, facecolor='blue', alpha=0.1)
+ax.plot(tickers, average_prices, c='green', alpha=0.5)
+ax.plot(tickers, current_prices, c='red', alpha=0.5)
 
 plt.title("Portfolio", fontsize=24)
 plt.xlabel("Ticker symbol", fontsize=10)
 plt.ylabel("Avg vs Current Price", fontsize=16)
-plt.tick_params(axis='both', which='major', labelsize=16)
+plt.tick_params(axis='both', which='major', labelsize=5)
 
    
 plt.show()
